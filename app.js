393a55ac0ebccc28cfbc3bb1d521406546f41f7e
jbirori <jeffbirori@gmail.com>
2018-02-18 19:06:32 -0600

    Made some progress on the log page. The current flow that I've implemented is prompting the user with the option to log either a workout or pain that they are experiencing. When they click on log pain it takes the user to the next step and presents them with a body map that allows them to specify which part of the body is hurting them. For now all this does is put a red mark where it hurts but does not really carry that data over to any of the other pages. They can then add in details about the pain they are experiencing and click on the log button. When they click on the log button it takes them to the stretch page. Ideally this button would also store the information in some sort of database that will carry the information to all other pages, such as the homepage where another body-map is and the timeline of workouts/pain. That is the next step.

---
+++ app.js
@@ -0,0 +1,29 @@
+function openLogPage() {
+  document.getElementById('log-selection').style.display = "none";
+  document.getElementById('log-pain').style.display = "block";
+}
+
+
+function toggleImage() {
+  var currImage = document.getElementById('body-map').attributes.src;
+  var currButton = document.getElementById('image-toggle');
+  if (currImage.value == "assets/front.png") {
+    currImage.value = "assets/back.png";
+    currButton.innerHTML = "Switch to Front"
+  }
+  else {
+    currImage.value = "assets/front.png";
+    currButton.innerHTML = "Switch to Back";
+  }
+
+}
+
+function markPain(e) {
+  var xPos = e.clientX;
+  var yPos = e.clientY;
+  var painPoint = document.createElement("DIV");
+  painPoint.setAttribute("class", "pain-points");
+  painPoint.setAttribute("style", ("top: " + yPos.toString() + "px; left: " + xPos.toString() + 'px;'));
+  document.getElementById("log-pain").appendChild(painPoint);
+
+}
