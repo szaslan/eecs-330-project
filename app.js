ea664db76be5032ae5782f235e4ce64b35db3e58
“szaslan” <SinanAslan2020@u.northwestern.edu>
2018-02-25 23:14:23 -0600

    added content to the Log workout section, still need to update the homepage whenever the log button is pressed. This isn't super attracitve but I'll be working on it still

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 //alert("Please open this app using one of the iPhone devices in the toolbar of Chrome's developer tools, that is what we have been using for development.");
 
-function openLogPage() {
+function openPainPage() {
   document.getElementById('log-selection').style.display = "none";
   document.getElementById('log-pain').style.display = "block";
 }
 
+function openWorkoutPage() {
+  document.getElementById('log-selection').style.display = "none";
+  document.getElementById('log-workout').style.display = "block";
+}
+
+
+function logWorkout(){
+  document.getElementById('timeline-day').innerHTML = "";
+}
 
 function toggleImage() {
   var currImage = document.getElementById('body-map').attributes.src;
